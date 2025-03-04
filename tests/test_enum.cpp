6991f016bf1f9f3ef52771bf7c81c860b4690917
Attrixx <148249434+Attrixx@users.noreply.github.com>
2025-03-04 14:35:16 +0100

    Update test_enum.cpp

--- tests/test_enum.cpp
+++ tests/test_enum.cpp
@@ -13,12 +13,20 @@ enum class Test : char
 int main() {
     std::cout << "Running Enum Tests...\n";
 
-    // Exemples de tests sur ta conversion enum -> string
+    // enum -> string
     assert(enum_to_string(Test::ONE) == "ONE");
     assert(enum_to_string(Test::TWO) == "TWO");
 
     std::string_view three = enum_to_string(Test::THREE);
     assert(three = "THREE");
+
+    // string -> enum
+    auto invalidOpt = string_to_enum<Test>("Whatever");
+    auto validOpt = string_to_enum<Test>("THREE");
+
+    assert(!invalidOpt.has_value());
+    assert(validOpt.has_value());
+    assert(*validOpt == Test::THREE);
 
     std::cout << "All tests passed!\n";
     return 0;
